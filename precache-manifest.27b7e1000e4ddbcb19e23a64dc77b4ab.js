58e33dcb7f4d1268bc47853bec998c3a7bb762af
kapilgupta101292 <kapilguptansit@gmail.com>
2020-01-25 16:31:04 +0530

    Updates

---
+++ precache-manifest.27b7e1000e4ddbcb19e23a64dc77b4ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3a6451bf092181866b85bdc0611d231",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "1490b2fb63113e154773",
+    "url": "/contactmanager/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b6c17b8387e5bfb9719c",
+    "url": "/contactmanager/static/css/main.dc31874e.chunk.css"
+  },
+  {
+    "revision": "1490b2fb63113e154773",
+    "url": "/contactmanager/static/js/2.d15e3b9e.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/contactmanager/static/js/2.d15e3b9e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b6c17b8387e5bfb9719c",
+    "url": "/contactmanager/static/js/main.4205813c.chunk.js"
+  },
+  {
+    "revision": "d86057960233a7428d99",
+    "url": "/contactmanager/static/js/runtime-main.2ce66dc9.js"
+  }
+]);
